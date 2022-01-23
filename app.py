d9e0dcfc3de85eadc2ee584cf3e04613d49b6936
Your Name <you@example.com>
2022-01-23 20:17:03 +0530

    code updated

--- app.py
+++ app.py
@@ -8,7 +8,6 @@
 from flask import Flask,render_template,jsonify,request
 from flask_cors import CORS,cross_origin
 import pandas as pd
-import numpy as np
 import joblib
 import xgboost
 from xgboost import XGBRegressor
@@ -18,8 +17,8 @@ from xgboost import XGBRegressor
 
 app=Flask(__name__)
 cors=CORS(app)
-scroe_predictor=pd.read_csv("artificats/file/cleaned.csv")
-model1=joblib.load('model','w+')
+scroe_predictor=pd.read_csv("artificats/file/cleaned_.csv")
+model1=joblib.load('finalmodel','w+')
 
 
 @app.route("/",methods=['GET','POST'])
@@ -44,15 +43,23 @@ def predict():
     wickets_left = 10 - wicketdown
     crr = (currentscore / over)
     # battingteams, bowlingteam,current_score,wickets_left,crr,city,last_five
+    try:
 
-    input_df = pd.DataFrame(
-        {'battingteam': [batting_team], 'bowlingteam': [bowling_team],'current_score': [currentscore],
-         'ball_count':[balls_count],'wickets_left': [wickets_left], 'crr': [crr],"city":[city] ,'last_five': [runs5]})
-    result = model1.predict(input_df)
-    print(result)
+        input_df = pd.DataFrame(
+            {'battingteam': [batting_team], 'bowlingteam': [bowling_team],'current_score': [currentscore],
+            'ball_count':[balls_count],'wickets_left': [wickets_left], 'crr': [crr],"city":[city] ,'last_five': [runs5]})
+        result = model1.predict(input_df)
+        print("jhal",result)
+        return  str(result)
+    except Exception as e:
+        print(e)
     #prediction=model.predict(pd.DataFrame(columns=['battingteams','bowlingteam','current_score','wickets_left', 'crr','city','last_five'],
                               #data=np.array([batting_team,bowling_team, currentscore, wicketdown, crr,city, runs5]).reshape(1, 7)))
-    return  str(result)
+    #input_df = pd.DataFrame(
+        #{'battingteam': ["Australia"], 'bowlingteam': ["srilanka"],'current_score': [97],
+        # 'ball_count':[45],'wickets_left': [7], 'crr': [8.67],"city":["Melbourne"] ,'last_five': [43]})
+    #result = model1.predict(input_df)                          
+    
     
     
 if __name__=="__main__":
